0eba10aab33f6e88d97f73e4a4b4d7ce1ef20add
Zaur Ibrahimov <zibrahimov95@gmail.com>
2020-07-05 19:55:50 +0400

    Redirect to Repo

--- src/components/home/RepoInfo.js
+++ src/components/home/RepoInfo.js
@@ -9,7 +9,7 @@ const RepoInfo = ({ repos }) => {
                 <div className="col-md-4 mb-3">
                     <div className="card h-100">
                         <div className="card-body">
-                            <h4 className="card-title">{repo.name}</h4>
+                            <h4 className="card-title"><a href={repo.html_url} target="_blank">{repo.name}</a></h4>
                             <p className="card-text">{repo.description}</p>
                             <span className="badge badge-dark mr-3">{repo.language}</span>
                             <span className="mr-3"><i className="far fa-star"></i> {repo.stargazers_count}</span>
